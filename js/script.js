5cba256245e1ef5c71e010e4dc2261da059b9688
Flor <florgoro07@gmail.com>
2024-08-19 12:18:09 -0300

    added-json

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok' + response.statusText);
+  }
+  return response.json();
+})
+
+.then (data => {
+  showData(data.students);
+})
+.catch (error => {
+  console.error('There was a problem', error);
+})
